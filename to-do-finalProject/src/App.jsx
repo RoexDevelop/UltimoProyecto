cfd333ff5638bf90b1cc1a669c743ccd4a8e2d48
RoexDevelop <roexcentral@gmail.com>
2026-01-17 20:04:48 +0100

    Filtros añadidos con lista filtrada

--- to-do-finalProject/src/App.jsx
+++ to-do-finalProject/src/App.jsx
@@ -3,25 +3,32 @@ import './style.css'
 import { useState, useEffect } from 'react'
 
 export function App () {
+    const [filters, setFilters] = useState([{id:Date.now()+(Math.random()*1000).toPrecision(5), nameFilter: 'All', isFiltering: true},
+        {id:Date.now()+(Math.random()*1000).toPrecision(5), nameFilter: 'Active', isFiltering: false},
+        {id:Date.now()+(Math.random()*1000).toPrecision(5), nameFilter: 'Completed', isFiltering: false}
+    ])
+    const activeFilter = filters.find(f => f.isFiltering)?.nameFilter // Para mi lista derivada (mostrar mis datos segun el tipo de iltro que esté en acción)
     const [inputTask, setInputTask] = useState('')
     const [tasks, setTasks] = useState([])
 
+
+    // --- Tasks ---
     function handleAddTask(e){
         e.preventDefault() //Evitar que el formulario recargue la página
         if (!inputTask?.trim()) return //Si inputTask es null o undefined, salir de la función
-        setTasks(prev => [...prev, {id:Date.now(), text:inputTask, done:false, isEditing:false, tempText:''}])
+        setTasks(prev => [...prev, {id:Date.now()+(Math.random()*1000).toPrecision(5), text:inputTask, done:false, isEditing:false, tempText:''}])
         setInputTask('')
     }
 
     {/**Completa una tarea al dar al checkbox o viceversa si le quita el check */}
     function completeTask(id){
-        const newArrayTasks = tasks.map((task) => {
-            if (task.id === id) {
-                return {...task, done: !task.done}
-            }
-            return task
-        })
-        setTasks(newArrayTasks)
+        // prev = previous state
+        setTasks(prev => prev.map(task => 
+            task.id === id
+                ? { ...task, done: !task.done }
+                : task
+            )
+        )
     }
 
     function deleteTask(id){
@@ -29,33 +36,29 @@ export function App () {
     }
 
     function editTask(id){
-        const newArrayTasks = tasks.map((task) => {
-            if (task.id === id) {
-                return{...task, isEditing: !task.isEditing, tempText: task.text}
-            }
-            return task
-        })
-        setTasks(newArrayTasks)
+        setTasks(prev => prev.map(task => 
+            task.id === id 
+                ? {...task, isEditing: !task.isEditing, tempText: task.text} 
+                : task
+            )
+
+        )
     }
 
     function changingText(text, id){
-        const newArrayTasks = tasks.map((task) =>{
-            if (task.id === id) {
-                return{...task, tempText: text }
-            }
-            return task
-        })
-        setTasks(newArrayTasks)
+        setTasks(prev => prev.map(task =>
+            task.id === id
+                ? {...task, tempText: text }
+                : task
+        ))
     }
 
     function cancelEditTask(id){
-        const newArrayTasks = tasks.map((task) => {
-            if (task.id === id) {
-                return {...task, isEditing: !task.isEditing, tempText: ''}
-            }
-            return task
-        })
-        setTasks(newArrayTasks) 
+        setTasks(prev => prev.map(task => 
+            task.id === id
+                ? {...task, isEditing: !task.isEditing, tempText: ''}
+                : task
+        ))
     }
 
     function updateTask(id){
@@ -68,9 +71,37 @@ export function App () {
         setTasks(newArrayTasks)
     }
 
+    // --- FILTERS FUNCTIONS --- 
+    function switchFilter(id){ 
+        setFilters(prev => prev.map(filter => ( 
+            {...filter, isFiltering: filter.id === id} 
+        )))
+    }
+
+    function getFilterCount(nameFilter){
+        if (nameFilter === 'All') return tasks.length
+        if (nameFilter === 'Active') return tasks.filter(task => !task.done).length
+        if (nameFilter === "Completed") return tasks.filter(task => task.done).length
+        return 0
+    }
+
+    // Lista filtrada
+    function getFilteredTasks(){
+        if (activeFilter === "Active") {
+            return tasks.filter(task => !task.done)
+        }
+
+        if (activeFilter === "Completed"){
+            return tasks.filter(task => task.done)
+        }
+
+        return tasks // -> All tasks
+    }
+
+
     useEffect(() =>{
-        console.log(tasks)
-    },[tasks])
+        // Cuerpo
+    },[tasks, filters])
     
     return (
     <div className='min-h-screen bg-gray-500 flex items-center justify-center'>
@@ -86,21 +117,29 @@ export function App () {
                     onChange={e => setInputTask(e.target.value)}
                     className='flex-1 px-3 py-2 border rounded bg-gray-100'/>
                 <button type='submit'
-                    className='border rounded-md bg-blue-500 text-white px-3 py-1 cursor-pointer'>
-                        Add</button>
+                        className='border rounded-md bg-blue-500 text-white px-3 py-1 cursor-pointer'>
+                    Add
+                </button>
             </form>
-
-
-            <h3>{inputTask}</h3>
+            {/**FILTROS */}
+            <nav className='flex justify-center gap-2 sm:gap-10'>
+                {filters.map((filter) => 
+                    <div key={filter.id} onClick={() => switchFilter(filter.id)}
+                        className={`flex flex-col items-center rounded px-8 py-1 cursor-pointer ${filter.isFiltering ? 'bg-gray-800 text-white' : 'bg-gray-100'}`}>
+                        <span className=''>{filter.nameFilter}</span>
+                        <span className='text-sm'>{getFilterCount(filter.nameFilter)}</span>
+                    </div>
+                )}
+            </nav>
             {/**Tasks */}
             <div id='divTasks' className='grid lg:grid-cols-2 gap-3'>
-                {tasks.map((task) =>
+                {getFilteredTasks().map((task) =>
                 // Div task
-                task.isEditing?
+                task.isEditing ?
                     //Si es true entra modo edición
                     (<div key={task.id} className='border-2 rounded border-gray-600 flex items-center gap-3 p-3'>
                         <input type="text"
-                            className='flex-1 p-1 border rounded'
+                            className='flex-1 p-1 border-2 border-blue-500 rounded'
                             value={task.tempText ?? ""} // Operador coalescencia nula: devuelve izquierda si NO! es null || undefined 
                             onChange={e => changingText(e.target.value, task.id)}/>
                         <button onClick={() => updateTask(task.id)}
